86c1bdae804b1699cf8c48c8f53c2c889f639a1e
Guilherme Biff Zarelli <gbzarelli@gmail.com>
2018-04-10 18:04:37 -0300

    Start Project

---
+++ app/src/main/java/br/com/helpdev/poollapscounter/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.com.helpdev.poollapscounter
+
+import android.app.Activity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        list_view.emptyView = text_view_empty
+    }
+}
