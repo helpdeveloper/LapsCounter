344369ad7e0a412b1871b329e32f5e4d26a03b96
Guilherme Biff Zarelli <gbzarelli@gmail.com>
2018-04-11 16:58:25 -0300

    Teste headset button

--- app/src/main/java/br/com/helpdev/poollapscounter/MainActivity.kt
+++ app/src/main/java/br/com/helpdev/poollapscounter/MainActivity.kt
@@ -1,12 +1,19 @@
 package br.com.helpdev.poollapscounter
 
+import android.content.ComponentName
+import android.content.Context
+import android.media.AudioManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
+import br.com.helpdev.poollapscounter.receiver.HeadsetButtonReceiver
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
+
 class MainActivity : AppCompatActivity() {
+    private var audioManager: AudioManager? = null
+    private var receiverComponent: ComponentName? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun registerHeadsetButton() {
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        receiverComponent = ComponentName(this, HeadsetButtonReceiver::class.java)
+        audioManager?.registerMediaButtonEventReceiver(receiverComponent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerHeadsetButton()
+    }
+
+    override fun onStop() {
+        if (null != receiverComponent) audioManager?.unregisterMediaButtonEventReceiver(receiverComponent)
+        super.onStop()
+    }
+
+    override fun onBackPressed() {
+        super.finish()
     }
 
 }
---
+++ app/src/main/java/br/com/helpdev/poollapscounter/receiver/HeadsetButtonReceiver.kt
@@ -0,0 +1,30 @@
+package br.com.helpdev.poollapscounter.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.view.KeyEvent
+import android.widget.Toast
+
+class HeadsetButtonReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent) {
+        val intentAction = intent.action
+        if (Intent.ACTION_MEDIA_BUTTON != intentAction) {
+            return
+        }
+        val event = intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT) as KeyEvent
+        val action = event.action
+
+        if (action == KeyEvent.ACTION_DOWN) {
+            val keycode = event.keyCode
+            val type = when (keycode) {
+                KeyEvent.KEYCODE_MEDIA_NEXT -> "next"
+                KeyEvent.KEYCODE_MEDIA_PREVIOUS -> "previous"
+                KeyEvent.KEYCODE_HEADSETHOOK -> "hook"
+                else -> "null"
+            }
+            Toast.makeText(context, "BUTTON PRESSED! {$type}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
