cf2f101d177e33fdbf45aad02139661f42c1ba16
Guilherme Biff Zarelli <gbzarelli@gmail.com>
2018-04-11 13:01:50 -0300

    Layout

--- app/src/main/java/br/com/helpdev/poollapscounter/MainActivity.kt
+++ app/src/main/java/br/com/helpdev/poollapscounter/MainActivity.kt
@@ -1,14 +1,24 @@
 package br.com.helpdev.poollapscounter
 
-import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
         list_view.emptyView = text_view_empty
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
