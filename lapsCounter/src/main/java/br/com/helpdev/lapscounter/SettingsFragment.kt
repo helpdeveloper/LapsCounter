06fe3d011a4b93f17c16012cb356c07b3d33eddd
Guilherme Biff Zarelli <gbzarelli@gmail.com>
2018-04-20 10:45:12 -0300

    Settings fragment

--- lapsCounter/src/main/java/br/com/helpdev/lapscounter/SettingsFragment.kt
+++ lapsCounter/src/main/java/br/com/helpdev/lapscounter/SettingsFragment.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import android.preference.Preference
 import android.preference.PreferenceFragment
 
-class SettingsFragment : PreferenceFragment(),Preference.OnPreferenceChangeListener{
+class SettingsFragment : PreferenceFragment(), Preference.OnPreferenceChangeListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.settings_preference)
+
+        val pref = preferenceManager.findPreference(getString(R.string.pref_lap_distance_name))
+        if (null != pref) {
+            pref.onPreferenceChangeListener = this
+            onPreferenceChange(pref,
+                    preferenceManager.sharedPreferences.getString(pref.key, getString(R.string.pref_lap_distance_default_value))
+            )
+        }
     }
 
-    override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        when (preference.key) {
+            getString(R.string.pref_lap_distance_name) -> preference.summary = getString(R.string.pref_lap_distance_summary, newValue)
+        }
         return true
     }
 
